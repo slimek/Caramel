5a79d30d5a190c528756ee98ce92a12929900890
slime573@gmail.com <slime573@gmail.com@41271ad6-4b3b-79c0-7981-d9d2cca8ec51>
2013-06-11 01:43:30 +0000

    add JsonValue (not implemented yet).
    git-svn-id: https://caramel-cpp.googlecode.com/svn/trunk@16 41271ad6-4b3b-79c0-7981-d9d2cca8ec51

---
+++ include/Caramel/Json/JsonValue.h
@@ -0,0 +1,30 @@
+// Caramel C++ Library - JSON Amenity - JSON Value Header
+
+#ifndef __CARAMEL_JSON_JSON_VALUE_H
+#define __CARAMEL_JSON_JSON_VALUE_H
+
+#include <Caramel/Caramel.h>
+
+#if defined( CARAMEL_COMPILER_HAS_PRAGMA_ONCE )
+#pragma once
+#endif
+
+
+namespace Caramel
+{
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// JSON Value
+//
+
+class JsonValue
+{
+};
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+} // namespace Caramel
+
+#endif // __CARAMEL_JSON_JSON_VALUE_H
