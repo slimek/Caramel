73ceda8ec073e2f50ca06d90e3b53b9fe927f183
slimek <slime573@gmail.com>
2013-09-13 14:52:08 +0800

    Add PchDefines.h and PchHeaders.h

---
+++ include/Caramel/Setup/PchDefines.h
@@ -0,0 +1,9 @@
+// Caramel C++ Library - Libary Setup - Precompiled-Header Defines
+
+#ifndef __CARAMEL_SETUP_PCH_DEFINES_H
+#define __CARAMEL_SETUP_PCH_DEFINES_H
+
+#include <Caramel/Setup/Compiler.h>
+#include <Caramel/Setup/System.h>
+
+#endif // __CARAMEL_SETUP_PCH_DEFINES_H
---
+++ include/Caramel/Setup/PchHeaders.h
@@ -0,0 +1,11 @@
+// Caramel C++ Library - Libary Setup - Precompiled-Header Headers
+
+#ifndef __CARAMEL_SETUP_PCH_HEADERS_H
+#define __CARAMEL_SETUP_PCH_HEADERS_H
+
+#include <Caramel/Error/Assert.h>
+#include <Caramel/Error/Exception.h>
+#include <Caramel/String/Sprintf.h>
+#include <Caramel/Trace/Trace.h>
+
+#endif // __CARAMEL_SETUP_PCH_HEADERS_H
