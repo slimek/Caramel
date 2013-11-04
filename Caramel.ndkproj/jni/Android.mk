9d7f90267b6491ecb664078c563323f63cb33d63
slimek <slime573@gmail.com>
2013-11-04 09:36:38 +0800

    Update NDK make files.

--- Caramel.ndkproj/jni/Android.mk
+++ Caramel.ndkproj/jni/Android.mk
@@ -4,44 +4,22 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := Caramel
 
+MY_SRC_DIR := ../../src
+
 LOCAL_SRC_FILES := \
-	../../src/Configuration.cpp \
-	../../src/DateTime.cpp \
-	../../src/Debug.cpp \
-	../../src/Document.cpp \
-	../../src/Error.cpp \
-	../../src/FileSystem.cpp \
-	../../src/Io.cpp \
-	../../src/Lexical.cpp \
-	../../src/Object.cpp \
-	../../src/Program.cpp \
-	../../src/Random.cpp \
-	../../src/Statechart.cpp \
-	../../src/String.cpp \
-	../../src/Task.cpp \
-	../../src/Thread.cpp \
-	../../src/Trace.cpp
+	$(MY_SRC_DIR)/Error.cpp \
+	$(MY_SRC_DIR)/Lexical.cpp \
+	$(MY_SRC_DIR)/Object.cpp \
+	$(MY_SRC_DIR)/Statechart.cpp \
+	$(MY_SRC_DIR)/String.cpp \
+	$(MY_SRC_DIR)/Task.cpp \
+	$(MY_SRC_DIR)/Thread.cpp \
+	$(MY_SRC_DIR)/Trace.cpp
 
 LOCAL_C_INCLUDES := \
 	$(LOCAL_PATH)/../../src \
 	$(LOCAL_PATH)/../../include \
 	$(GIT_ROOT)/CandyJar/include
 	
-LOCAL_WHOLE_STATIC_LIBRARIES := boost-chrono
-LOCAL_WHOLE_STATIC_LIBRARIES += boost-filesystem
-LOCAL_WHOLE_STATIC_LIBRARIES += boost-program_options
-LOCAL_WHOLE_STATIC_LIBRARIES += boost-system
-LOCAL_WHOLE_STATIC_LIBRARIES += boost-thread
-
-include $(BUILD_SHARED_LIBRARY)
-
-
-$(call import-add-path,$(GIT_ROOT)/CandyJar)
-
-$(call import-module,Boost.Chrono)
-$(call import-module,Boost.FileSystem)
-$(call import-module,Boost.ProgramOptions)
-$(call import-module,Boost.System)
-$(call import-module,Boost.Thread)
-
+include $(BUILD_STATIC_LIBRARY)
  
