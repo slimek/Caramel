c34bdf055c877cbd5222edac0f4d13dcf050bde3
slimek <slime573@gmail.com>
2013-11-05 00:14:23 +0800

    Add SpinMutex class.

---
+++ include/Caramel/Thread/SpinMutex.h
@@ -0,0 +1,62 @@
+// Caramel C++ Library - Thread Facility - Spin Mutex Header
+
+#ifndef __CARAMEL_THREAD_SPIN_MUTEX_H
+#define __CARAMEL_THREAD_SPIN_MUTEX_H
+
+#include <Caramel/Caramel.h>
+
+#if defined( CARAMEL_COMPILER_HAS_PRAGMA_ONCE )
+#pragma once
+#endif
+
+#include <boost/noncopyable.hpp>
+#include <boost/smart_ptr/detail/spinlock_pool.hpp>
+
+
+namespace Caramel
+{
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Spin Mutex
+// - This is a wrapper of Boost.SmartPtr spinlock.
+//
+
+class SpinMutex : public boost::noncopyable
+{
+public:
+
+    class ScopedLock
+    {
+    public:
+        ScopedLock( SpinMutex& mutex );
+        ~ScopedLock();
+
+    private:
+        boost::detail::spinlock& m_lock;
+    };
+};
+
+
+//
+// Implementation
+//
+
+inline SpinMutex::ScopedLock::ScopedLock( SpinMutex& mutex )
+    : m_lock( boost::detail::spinlock_pool< 0 >::spinlock_for( &mutex ))
+{
+    m_lock.lock();
+}
+
+
+inline SpinMutex::ScopedLock::~ScopedLock()
+{
+    m_lock.unlock();
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+} // namespace Caramel
+
+#endif // __CARAMEL_THREAD_SPIN_MUTEX_H
---
+++ test/Thread/SpinMutexTest.cpp
@@ -0,0 +1,73 @@
+// Caramel C++ Library Test - Thread - Spin Mutex Test
+
+#include "CaramelTestPch.h"
+
+#include <Caramel/Thread/SpinMutex.h>
+#include <Caramel/Thread/Thread.h>
+#include <UnitTest++/UnitTest++.h>
+
+
+namespace Caramel
+{
+
+using namespace std;
+
+SUITE( SpinMutexSuite )
+{
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Spin Mutex Test
+//
+
+SpinMutex s_mutex;
+Int s_value = 0;
+Int s_count = 0;
+
+const Uint LOOP = 100000;
+
+void Execute()
+{
+    for ( Uint i = 0; i < LOOP; ++ i )
+    {
+        SpinMutex::ScopedLock lock( s_mutex );
+        ++ s_value;
+        CHECK( 1 == s_value );
+        ++ s_count;
+        -- s_value;
+    }
+}
+
+
+TEST( SpinMutexTest )
+{
+    /// Recover Test ///
+
+    {
+        SpinMutex::ScopedLock lock( s_mutex );
+    }
+
+    {
+        SpinMutex::ScopedLock lock( s_mutex );
+    }
+
+
+    /// Race Test ///
+
+    Thread t1( "Exec1", [=] { Execute(); } );
+    Thread t2( "Exec2", [=] { Execute(); } );
+    Thread t3( "Exec3", [=] { Execute(); } );
+
+    t1.Join();
+    t2.Join();
+    t3.Join();
+
+    CHECK( s_count == LOOP * 3 );
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+} // SUITE SpinMutexSuite
+
+} // namespace Caramel
