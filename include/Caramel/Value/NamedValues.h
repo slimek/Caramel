439a2f59aae77468c21db7add959617f14deb603
slimek <slime573@gmail.com>
2013-11-29 09:49:53 +0800

    (minor) Rearrange include headers.

--- include/Caramel/Value/NamedValues.h
+++ include/Caramel/Value/NamedValues.h
@@ -2,13 +2,9 @@
 
 #ifndef __CARAMEL_VALUE_NAMED_VALUES_H
 #define __CARAMEL_VALUE_NAMED_VALUES_H
+#pragma once
 
 #include <Caramel/Caramel.h>
-
-#if defined( CARAMEL_COMPILER_HAS_PRAGMA_ONCE )
-#pragma once
-#endif
-
 #include <Caramel/Value/Detail/NamedValueRef.h>
 #include <map>
 
