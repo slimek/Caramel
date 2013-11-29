eb142a8435449ca1039fd595b26d6d7a8507d340
slimek <slime573@gmail.com>
2013-11-29 17:50:58 +0800

    Fix: Don't assert sizeof( Long ) in Xcode.

--- src/CaramelPch.cpp
+++ src/CaramelPch.cpp
@@ -17,8 +17,15 @@ namespace Caramel
 
 static_assert( 4 == sizeof( Int ),   "sizeof( Int ) should be 4" );
 static_assert( 4 == sizeof( Uint ),  "sizeof( Uint ) should be 4" );
+
+#if !defined( CARAMEL_SYSTEM_IS_IOS )
+
+// NOTES: In Xcode we can't get the size of Long/Ulong. The reason is unknown yet...
+
 static_assert( 4 == sizeof( Long ),  "sizeof( Long ) should be 4" );
 static_assert( 4 == sizeof( Ulong ), "sizeof( Ulong ) should be 4" );
+
+#endif
 
 
 ///////////////////////////////////////////////////////////////////////////////
