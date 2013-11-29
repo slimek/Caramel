935d40f5e1e03ae61ddd243745294a84b03a4340
shuai.tseng <himura__kenshin@hotmail.com>
2013-11-29 17:39:24 +0800

    remove static_assert for long.

--- src/CaramelPch.cpp
+++ src/CaramelPch.cpp
@@ -17,8 +17,6 @@ namespace Caramel
 
 static_assert( 4 == sizeof( Int ),   "sizeof( Int ) should be 4" );
 static_assert( 4 == sizeof( Uint ),  "sizeof( Uint ) should be 4" );
-static_assert( 4 == sizeof( Long ),  "sizeof( Long ) should be 4" );
-static_assert( 4 == sizeof( Ulong ), "sizeof( Ulong ) should be 4" );
 
 
 ///////////////////////////////////////////////////////////////////////////////
