1a814397af3b035ff50387ead2c72da5918db738
slimek <slime573@gmail.com>
2013-12-02 23:45:18 +0800

    Add Setup/Version.h header.

---
+++ include/Caramel/Setup/Version.h
@@ -0,0 +1,40 @@
+// Caramel C++ Library - Libary Setup - Version Desination
+
+#ifndef __CARAMEL_SETUP_VERSION_H
+#define __CARAMEL_SETUP_VERSION_H
+
+
+//
+// Current Version
+//
+
+#define CARAMEL_VERSION_MAJOR 1
+#define CARAMEL_VERSION_MINOR 0
+#define CARAMEL_VERSION_BUILD 0
+
+
+//
+// Version Validation Example
+// - In Debug, minor version should be backwards compatibility.
+//   In Release, all numbers must be matched.
+//
+// #if defined( NDEBUG )
+//
+// static_assert(
+//   CARAMEL_VERSION_MAJOR == 1 &&
+//   CARAMEL_VERSION_MINOR == 0 &&
+//   CARAMEL_VERSION_BUILD == 0,
+//   "Caramel version not matched" );
+//
+// #else
+//
+// static_assert(
+//   CARAMEL_VERSION_MAJOR == 1 &&
+//   CARAMEL_VERSION_MINOR >= 0,
+//   "Caramel version not compatible );
+//
+// #endif // NDEBUG
+//
+
+
+#endif // __CARAMEL_SETUP_VERSION_H
