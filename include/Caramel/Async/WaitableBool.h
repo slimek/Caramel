c1461120c84e0a827efd252105c0c74cbe5e13db
slimek <slime573@gmail.com>
2013-12-08 22:47:07 +0800

    Add WaitableBool class.

---
+++ include/Caramel/Async/WaitableBool.h
@@ -0,0 +1,109 @@
+// Caramel C++ Library - Async Amenity - Waitable Bool Header
+
+#ifndef __CARAMEL_ASYNC_WAITABLE_BOOL_H
+#define __CARAMEL_ASYNC_WAITABLE_BOOL_H
+#pragma once
+
+#include <Caramel/Caramel.h>
+#include <Caramel/Thread/MutexLocks.h>
+#include <condition_variable>
+#include <mutex>
+
+
+namespace Caramel
+{
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Waitable Bool
+// - Block a thread until this boolean becomes true.
+//
+
+class WaitableBool
+{
+public:
+
+    WaitableBool();
+    explicit WaitableBool( Bool initialValue );
+
+    
+    /// Operators ///
+
+    WaitableBool& operator=( Bool value );
+
+
+    /// Accessors ///
+
+    operator Bool() const { return m_value; }
+
+
+    /// Manipulators ///
+
+    void Wait();
+
+
+private:
+
+    Bool m_value;
+
+    std::mutex m_mutex;
+    std::condition_variable m_becomesTrue;
+};
+
+
+//
+// Implementation
+//
+
+inline WaitableBool::WaitableBool()
+    : m_value( false )
+{
+}
+
+
+inline WaitableBool::WaitableBool( Bool initialValue )
+    : m_value( initialValue )
+{
+}
+
+
+/// Operators ///
+
+inline WaitableBool& WaitableBool::operator=( Bool value )
+{
+    Bool oldValue = false;
+
+    {
+        auto ulock = UniqueLock( m_mutex );
+        oldValue = m_value;
+        m_value = value;
+    }
+
+    if ( ! oldValue && value )
+    {
+        // false => true
+        m_becomesTrue.notify_all();
+    }
+
+    return *this;
+}
+
+
+/// Manipulators ///
+
+inline void WaitableBool::Wait()
+{
+    auto ulock = UniqueLock( m_mutex );
+
+    while ( ! m_value )
+    {
+        m_becomesTrue.wait( ulock );
+    }
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+} // namespace Caramel
+
+#endif // __CARAMEL_ASYNC_WAITABLE_BOOL_H
---
+++ test/src/Async/WaitableBoolTest.cpp
@@ -0,0 +1,43 @@
+// Caramel C++ Library Test - Async - Waitable Boolean Test
+
+#include "CaramelTestPch.h"
+
+#include <Caramel/Async/WaitableBool.h>
+#include <Caramel/Thread/Thread.h>
+#include <Caramel/Thread/ThisThread.h>
+#include <UnitTest++/UnitTest++.h>
+
+
+namespace Caramel
+{
+
+SUITE( WaitableBoolSuite )
+{
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Waitable Bool Test
+//
+
+TEST( WaitableBoolTest )
+{
+    // If the test failed, program would be blocked forever.
+
+    WaitableBool flag( false );
+
+    Thread t1( "Test1", [&]
+    {
+        flag.Wait();
+    });
+
+    ThisThread::SleepFor( Ticks( 500 ));
+    flag = true;
+    t1.Join();
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+} // SUITE WaitableBoolSuite
+
+} // namespace Caramel
