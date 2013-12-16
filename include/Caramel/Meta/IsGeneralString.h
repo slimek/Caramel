847b6ebd4b689af7577cdcc800630991869829d6
slimek <slime573@gmail.com>
2013-12-17 00:50:37 +0800

    Add Meta/IsGeneralString.h

---
+++ include/Caramel/Meta/IsGeneralString.h
@@ -0,0 +1,41 @@
+// Caramel C++ Library - Metaprogramming Amenity - Is General String Header
+
+#ifndef __CARAMEL_META_IS_GENERAL_STRING_H
+#define __CARAMEL_META_IS_GENERAL_STRING_H
+#pragma once
+
+#include <Caramel/Caramel.h>
+#include <Caramel/Meta/Utility.h>
+
+
+namespace Caramel
+{
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Is General String
+// - True if T is :
+//   std::string
+//   C-style string - char* and char[].
+//
+
+template< typename T >
+struct IsGeneralString
+    : public BoolType
+        <
+            std::is_same< Char*,       typename std::decay< T >::type >::value
+         || std::is_same< const Char*, typename std::decay< T >::type >::value
+        >
+{};
+
+
+template<>
+struct IsGeneralString< std::string > : public TrueType
+{};
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+} // namespace Caramel
+
+#endif // __CARAMEL_META_IS_GENERAL_STRING_H
