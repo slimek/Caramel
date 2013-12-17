9543fcd051775ae5e5993546c0acd14118662a84
slimek <slime573@gmail.com>
2013-12-17 23:51:01 +0800

    Add NumericTraits::CanExactConvert().

--- include/Caramel/Numeric/NumberTraits.h
+++ include/Caramel/Numeric/NumberTraits.h
@@ -78,9 +78,34 @@ struct NumberTraits< Float >
     // Exact Integer Limits
     // - You may convert these integers to Double without losing precision.
     //
+    
     static const Int32  MAX_EXACT_INT32  = 0xFFFFFF;  // 24 bits
     static const Int32  MIN_EXACT_INT32  = - MAX_EXACT_INT32;
     static const Uint32 MAX_EXACT_UINT32 = MAX_EXACT_INT32;
+
+
+    //
+    // Conversion Traits
+    //
+
+    template< typename T >
+    static Bool CanExactConvert( T value )
+    {
+        static_assert( std::is_integral< T >::value, "T must be integral type" );
+        return true;
+    }
+
+    template<>
+    static Bool CanExactConvert< Int64 >( Int64 value )
+    {
+        return MIN_EXACT_INT32 <= value && value <= MAX_EXACT_INT32;
+    }
+
+    template<>
+    static Bool CanExactConvert< Uint64 >( Uint64 value )
+    {
+        return value <= MAX_EXACT_UINT32;
+    }
 };
 
 
@@ -92,9 +117,34 @@ struct NumberTraits< Double >
     // Exact Integer Limits
     // - You may convert these integers to Double without losing precision.
     //
+    
     static const Int64  MAX_EXACT_INT64  = 0x1FFFFFFFFFFFFFll;  // 53 bits
     static const Int64  MIN_EXACT_INT64  = - MAX_EXACT_INT64;
     static const Uint64 MAX_EXACT_UINT64 = MAX_EXACT_INT64;
+
+
+    //
+    // Conversion Traits
+    //
+
+    template< typename T >
+    static Bool CanExactConvert( T value )
+    {
+        static_assert( std::is_integral< T >::value, "T must be integral type" );
+        return true;
+    }
+
+    template<>
+    static Bool CanExactConvert< Int64 >( Int64 value )
+    {
+        return MIN_EXACT_INT64 <= value && value <= MAX_EXACT_INT64;
+    }
+
+    template<>
+    static Bool CanExactConvert< Uint64 >( Uint64 value )
+    {
+        return value <= MAX_EXACT_UINT64;
+    }
 };
 
 
