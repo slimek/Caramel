2ffb6d5c75ae4d0a6317c910197e9af43f44b530
slimek <slime573@gmail.com>
2014-03-06 11:31:40 +0800

    [Numeric] Add UnionBits16 and UnionBits32 unions.

---
+++ include/Caramel/Numeric/UnionBits.h
@@ -0,0 +1,75 @@
+// Caramel C++ Library - Numeric Amenity - Union Bits Header
+
+#ifndef __CARAMEL_NUMERIC_UNION_BITS_H
+#define __CARAMEL_NUMERIC_UNION_BITS_H
+#pragma once
+
+#include <Caramel/Caramel.h>
+
+
+namespace Caramel
+{
+
+////////////////////////////////////////////////////////////////////////////
+//
+// Union Bits
+// - Fundamental types compound.
+//   
+//   ATTENTION:
+//     Current implementation only works in little endian architectures.
+//
+//   TODO:
+//     After the Boost is upgraded to v1.55 or higher,
+//     use Boost.Predef macro BOOST_ENDIAN_LITTLE_BYTE to verify the endian.
+//
+
+//
+// 16 Bits
+//
+
+// Components
+struct CompBiByte { Byte lo, hi; };  // Low, High
+
+union UnionBits16
+{
+    Int16   i;
+    Uint16  ui;
+    Byte    bs[2];
+
+    CompBiByte bb;
+};
+
+
+//
+// 32 Bits
+//
+
+// Components
+struct CompQuByte { Byte lo, ml, mh, hi; };  // Low, Middle-low, Middle-high, High
+struct CompBiBits16 { UnionBits16 lo, hi; };
+
+union UnionBits32
+{
+    Int32   i;
+    Uint32  ui;
+    Float   f;
+    Byte    bs[4];
+
+    CompQuByte   qb;
+    CompBiBits16 bb16;
+};
+
+
+//
+// Verification
+//
+
+static_assert( 2 == sizeof( UnionBits16 ), "UnionBits16 should be 16-bit" );
+static_assert( 4 == sizeof( UnionBits32 ), "UnionBits32 should be 32-bit" );
+
+
+////////////////////////////////////////////////////////////////////////////
+
+} // namespace Caramel
+
+#endif // __CARAMEL_NUMERIC_UNION_BITS_H
---
+++ test/src/Numeric/UnionBitsTest.cpp
@@ -0,0 +1,75 @@
+// Caramel C++ Library Test - Numeric - Union Bits Test
+
+#include "CaramelTestPch.h"
+
+#include <Caramel/Numeric/UnionBits.h>
+#include <UnitTest++/UnitTest++.h>
+
+
+namespace Caramel
+{
+
+SUITE( UnionBitsSuite )
+{
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Union Bits Test
+//
+
+TEST( UnionBits16Test )
+{
+    UnionBits16 b16;
+    b16.i = 0x1234;
+
+    CHECK( 0x1234 == b16.ui );
+    CHECK( 0x34   == b16.bs[0] );
+    CHECK( 0x12   == b16.bs[1] );
+    CHECK( 0x34   == b16.bb.lo );
+    CHECK( 0x12   == b16.bb.hi );
+}
+
+
+TEST( UnionBits32Test )
+{
+    UnionBits32 b32;
+    b32.i = 0x12345678;
+
+    CHECK( 0x12345678u == b32.ui );
+
+    // Bytes
+    CHECK( 0x78 == b32.bs[0] );
+    CHECK( 0x56 == b32.bs[1] );
+    CHECK( 0x34 == b32.bs[2] );
+    CHECK( 0x12 == b32.bs[3] );
+
+    // Qu-Byte
+    CHECK( 0x78 == b32.qb.lo );
+    CHECK( 0x56 == b32.qb.ml );
+    CHECK( 0x34 == b32.qb.mh );
+    CHECK( 0x12 == b32.qb.hi );
+
+    // Bi-Bits16
+    CHECK( 0x5678 == b32.bb16.lo.ui );
+    CHECK( 0x1234 == b32.bb16.hi.ui );
+
+
+    /// Floating from/to Binary Presentation ///
+
+    UnionBits32 f32;
+    f32.ui = 0x3F800000;
+
+    CHECK( 1.0f == f32.f );
+
+    f32.f = -1.0f;
+
+    CHECK( 0xBF800000u == f32.ui );
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+} // SUITE UnionBitsSuite
+
+} // namespace Caramel
+
