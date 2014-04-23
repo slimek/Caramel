0dc63a39b812f3c9c1e28a18b1bd919710bbc146
slimek <slime573@gmail.com>
2014-04-23 23:43:57 +0800

    [Random] (minor) Add test for WeightedRandomMap with Float weight.

--- test/src/Random/WeightedRandomMapTest.cpp
+++ test/src/Random/WeightedRandomMapTest.cpp
@@ -14,29 +14,54 @@ SUITE( WeightedRandomMapSuite )
 
 ///////////////////////////////////////////////////////////////////////////////
 //
-// Test
+// Weighted Random Map Test
 //
 
 TEST( WeightedRandomMapTest )
 {
-    WeightedRandomMap< Int, Int > map;
-
-    map.Add( 1,  0 );
-    map.Add( 9,  1 );
-    map.Add( 90, 2 );
-
-    Int counts[3] = { 0 };
-
-    const Int BIAS = 1000;
-
-    for ( Int i = 0; i < BIAS * 100; ++ i )
+    // Weight is Int
     {
-        ++ counts[ map.Next() ];
+        WeightedRandomMap< Int, Int > map;
+
+        map.Add( 1,  0 );
+        map.Add( 9,  1 );
+        map.Add( 90, 2 );
+
+        Int counts[3] = { 0 };
+
+        const Int BIAS = 1000;
+
+        for ( Int i = 0; i < BIAS * 100; ++ i )
+        {
+            ++ counts[ map.Next() ];
+        }
+
+        CHECK_CLOSE(      BIAS, counts[0], BIAS / 10);
+        CHECK_CLOSE(  9 * BIAS, counts[1], BIAS );
+        CHECK_CLOSE( 90 * BIAS, counts[2], BIAS );
     }
 
-    CHECK_CLOSE(      BIAS, counts[0], BIAS / 10);
-    CHECK_CLOSE(  9 * BIAS, counts[1], BIAS );
-    CHECK_CLOSE( 90 * BIAS, counts[2], BIAS );
+    // Weight is Float
+    {
+        WeightedRandomMap< Float, Int > map;
+
+        map.Add( 0.01f, 0 );
+        map.Add( 0.09f, 1 );
+        map.Add( 0.9f, 2 );
+
+        Int counts[3] = { 0 };
+
+        const Int BIAS = 1000;
+
+        for ( Int i = 0; i < BIAS * 100; ++ i )
+        {
+            ++ counts[ map.Next() ];
+        }
+
+        CHECK_CLOSE(      BIAS, counts[0], BIAS / 10);
+        CHECK_CLOSE(  9 * BIAS, counts[1], BIAS );
+        CHECK_CLOSE( 90 * BIAS, counts[2], BIAS );
+    }
 }
 
 
