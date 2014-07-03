60ce24a957371de96bcb799c6b77c8585cafd5fe
slimek <slime573@gmail.com>
2014-07-03 10:41:39 +0800

    [Concurrent] Add Stack class.

---
+++ include/Caramel/Concurrent/Stack.h
@@ -0,0 +1,102 @@
+// Caramel C++ Library - Concurrent Amenity - Stack Header
+
+#ifndef __CARAMEL_CONCURRENT_STACK_H
+#define __CARAMEL_CONCURRENT_STACK_H
+#pragma once
+
+#include <Caramel/Setup/CaramelDefs.h>
+#include <Caramel/Thread/MutexLocks.h>
+#include <boost/noncopyable.hpp>
+#include <deque>
+
+
+namespace Caramel
+{
+
+namespace Concurrent
+{
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Concurrent Stack
+//
+
+template< typename T >
+class Stack : public boost::noncopyable
+{
+public:
+
+    typedef T ValueType;
+
+    
+    /// Properties ///
+
+    Bool IsEmpty() const { return m_stack.empty(); }
+    Uint Size()    const { return static_cast< Uint >( m_stack.size() ); }
+
+
+    /// Operations ///
+
+    void Push( const T& x );
+    void Push( T&& x );
+
+    Bool TryPop( T& x );
+
+
+private:
+
+    /// Data Members ///
+
+    typedef std::deque< T > StackType;
+    StackType m_stack;
+
+    mutable std::mutex m_stackMutex;
+};
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Implementation
+//
+
+template< typename T >
+inline void Stack< T >::Push( const T& x )
+{
+    LockGuard lock( m_stackMutex );
+
+    m_stack.push_back( x );
+}
+
+
+template< typename T >
+inline void Stack< T >::Push( T&& x )
+{
+    LockGuard lock( m_stackMutex );
+
+    m_stack.push_back( std::move( x ));
+}
+
+
+template< typename T >
+inline Bool Stack< T >::TryPop( T& x )
+{
+    if ( m_stack.empty() ) { return false; }
+
+    LockGuard lock( m_stackMutex );
+
+    if ( m_stack.empty() ) { return false; }
+
+    x = m_stack.back();
+    m_stack.pop_back();
+
+    return true;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+} // namespace Concurrent
+
+} // namespace Caramel
+
+#endif // __CARAMEL_CONCURRENT_STACK_H
---
+++ test/src/Concurrent/ConcurrentStackSuite.cpp
@@ -0,0 +1,104 @@
+// Caramel C++ Library Test - Concurrent - Stack Suite
+
+#include "CaramelTestPch.h"
+
+#include <Caramel/Chrono/TimedBool.h>
+#include <Caramel/Concurrent/Stack.h>
+#include <Caramel/Thread/Thread.h>
+#include <UnitTest++/UnitTest++.h>
+#include <atomic>
+
+
+namespace Caramel
+{
+
+SUITE( ConcurrentStackSuite )
+{
+
+TEST( ConcurrentStackTest )
+{
+    Concurrent::Stack< Int > istack;
+    Int value = 0;
+
+    /// Trivial ///
+
+    CHECK( true  == istack.IsEmpty() );
+    CHECK( 0     == istack.Size() );
+    CHECK( false == istack.TryPop( value ));
+
+
+    /// Push and Pop 1 values ///
+
+    istack.Push( 42 );
+
+    CHECK( false == istack.IsEmpty() );
+    CHECK( 1     == istack.Size() );
+    CHECK( true  == istack.TryPop( value ));
+    CHECK( 42    == value );
+
+    CHECK( true  == istack.IsEmpty() );
+    CHECK( 0     == istack.Size() );
+    CHECK( false == istack.TryPop( value ));
+
+
+    /// Push and Pop a sequence ///
+
+    istack.Push( 16 );
+    istack.Push( 64 );
+    istack.Push( 256 );
+
+    CHECK( false == istack.IsEmpty() );
+    CHECK( 3     == istack.Size() );
+
+    CHECK( true  == istack.TryPop( value ));
+    CHECK( 256   == value );
+    CHECK( true  == istack.TryPop( value ));
+    CHECK( 64    == value );
+    CHECK( true  == istack.TryPop( value ));
+    CHECK( 16    == value );
+    CHECK( false == istack.TryPop( value ));
+}
+
+
+TEST( ConcurrentStackWarmupTest )
+{
+    Concurrent::Stack< std::string > stack;
+
+    TimedBool< TickClock > timeup( 1000 );
+
+    std::atomic< Int > pushes( 0 );
+    std::atomic< Int > pops( 0 );
+
+    Thread t1( "Pusher",
+    [&]
+    {
+        while ( ! timeup )
+        {
+            stack.Push( "Cirno" );
+            ++ pushes;
+        }
+    });
+
+    Thread t2( "Popper",
+    [&]
+    {
+        while ( ! timeup )
+        {
+            std::string value;
+            if ( stack.TryPop( value ))
+            {
+                ++ pops;
+            }
+        }
+    });
+
+    t1.Join();
+    t2.Join();
+
+    CARAMEL_TRACE_DEBUG( "Pushes: %d, Pops: %d", pushes.load(), pops.load() );
+}
+
+
+} // SUITE ConcurrentStackSuite
+
+} // namespace Caramel
