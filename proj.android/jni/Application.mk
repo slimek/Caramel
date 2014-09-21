da2a728a339d756f6a7a7d9a5e5738c7641fd0d0
slimek <slime573@gmail.com>
2014-09-21 22:29:12 +0800

    [Android] Improve project settings. Now it can make a prebuilt static library.

---
+++ proj.android/jni/Application.mk
@@ -0,0 +1,24 @@
+APP_PLATFORM := android-15
+APP_ABI := x86
+APP_STL := gnustl_static
+
+APP_CFLAGS := -fexceptions
+APP_CPPFLAGS := -std=c++11 -frtti
+
+ifeq ($(NDEBUG),1)
+
+# Release
+APP_OPTIM := release
+CARAMEL_CONFIG := Release
+
+else
+
+# Debug
+APP_OPTIM := debug
+CARAMEL_CONFIG := Debug
+APP_CFLAGS += -g
+
+endif
+
+NDK_APP_OUT := obj/$(CARAMEL_CONFIG)
+NDK_TOOLCHAIN_VERSION := 4.8
