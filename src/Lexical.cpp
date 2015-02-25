9018d12fe2cccced8c154bb828f68febe830cf8c
slimek <slime573@gmail.com>
2015-02-25 14:03:37 +0800

    [VC] (refactoring) Use strtoll/strtoull().
    - VC 2013 has supported them.

--- src/Lexical.cpp
+++ src/Lexical.cpp
@@ -144,16 +144,7 @@ Bool Integer< Uint32 >::TryParse( const std::string& input )
 
 //
 // Convert Int64 to String
-// - Visual C++ doesn't support strtoull() until 2013.
 //
-
-#if defined( CARAMEL_COMPILER_IS_MSVC )
-#define CARAMEL_STRTOLL  _strtoi64
-#define CARAMEL_STRTOULL _strtoui64
-#else
-#define CARAMEL_STRTOLL  strtoll
-#define CARAMEL_STRTOULL strtoull
-#endif
 
 template<>
 Bool Integer< Int64 >::TryParse( const std::string& input )
@@ -164,11 +155,11 @@ Bool Integer< Int64 >::TryParse( const std::string& input )
 
     if ( CainStartsWith( input, "0x" ))
     {
-        m_value = static_cast< Int64 >( CARAMEL_STRTOULL( input.c_str(), &stop, 16 ));
+        m_value = static_cast< Int64 >( ::strtoull( input.c_str(), &stop, 16 ));
     }
     else
     {
-        m_value = static_cast< Int64 >( CARAMEL_STRTOLL( input.c_str(), &stop, 10 ));
+        m_value = static_cast< Int64 >( ::strtoll( input.c_str(), &stop, 10 ));
     }
     
     return stop == ( input.data() + input.length() );
@@ -184,11 +175,11 @@ Bool Integer< Uint64 >::TryParse( const std::string& input )
 
     if ( CainStartsWith( input, "0x" ))
     {
-        m_value = static_cast< Uint64 >( CARAMEL_STRTOULL( input.c_str(), &stop, 16 ));
+        m_value = static_cast< Uint64 >( ::strtoull( input.c_str(), &stop, 16 ));
     }
     else
     {
-        m_value = static_cast< Uint64 >( CARAMEL_STRTOULL( input.c_str(), &stop, 10 ));
+        m_value = static_cast< Uint64 >( ::strtoull( input.c_str(), &stop, 10 ));
     }
     
     return stop == ( input.data() + input.length() );
